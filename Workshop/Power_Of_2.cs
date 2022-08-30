6077bb1bdd6bdad1eb021947254f328f27107ca7
[ganapat] <ganapatisakatwa@gmail.com>
2022-08-30 09:15:34 +0530

    [Ganapati] Power Of 2

---
+++ Workshop/Power_Of_2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop
+{
+    public class Power_Of_2
+    {
+        public void Pow_2()
+        {
+            Console.WriteLine("Enter any Positive integer to find the Power of 2");
+            int power =Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Your find 2 Power {0}, power");
+            double pow = 0;
+
+            for (int i=1;i<=power; i++)
+            {
+                pow = Math.Pow(2, power);
+            }
+
+            Console.WriteLine("The power of 2 is"+pow);
+        }
+    }
+}
---
+++ Workshop/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Workshop
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Power_Of_2 obj = new Power_Of_2();
+            obj.Pow_2();
+        }
+    }
+}
